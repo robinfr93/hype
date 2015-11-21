1d8add2285717127602d3f3b3c611074d8207b28
Nitin Paul <npaul2811@gmail.com>
2015-11-21 21:56:19 +0530

    Added database settings

--- hype/hype/settings.py
+++ hype/hype/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'hype.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hype',
+        'USER': 'hype_admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
