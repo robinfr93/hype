b49d6b4c3fc89a0eec1b76e458e9dd49e87207ff
robinfr93 <robinfr93@gmail.com>
2015-11-23 23:23:35 +0530

    A ddu pull request

--- hype/blog/models.py
+++ hype/blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+# this is just a comment for a dud pull request, ignore it and under no circumstance merge this request
+# thank you for taking out the time to read this
 
 class Tag(models.Model):
     slug = models.SlugField(max_length=255, blank=True)
@@ -15,7 +17,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey('Post')
     body = models.TextField()
-    published_on = models.DateTimeField(auto_now=True ) 
+    published_on = models.DateTimeField(auto_now=True )
     published = models.BooleanField(default=False)
     slug = models.SlugField(max_length=255, blank=True)
     tag = models.ManyToManyField(Tag, related_name="post")
