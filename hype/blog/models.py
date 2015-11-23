f347260b068212d92d0e769e8db7691543cd6760
vagrant <vagrant@precise32.(none)>
2015-11-23 17:25:08 +0000

    Added foreign key relation for Author

--- hype/blog/models.py
+++ hype/blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Tag(models.Model):
     slug = models.SlugField(max_length=255, blank=True)
-    
 
     def __str__(self):
         return self.slug
@@ -12,15 +13,19 @@ class Tag(models.Model):
 class Post(models.Model):
     """ Model for post """
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=100,)
+    author = models.ForeignKey('Post')
     body = models.TextField()
     published_on = models.DateTimeField(auto_now=True)
     published = models.BooleanField(default=False)
-    slug  = models.SlugField(max_length=255, blank=True)
+    slug = models.SlugField(max_length=255, blank=True)
     tag = models.ManyToManyField(Tag, related_name="post")
 
     def __str__(self):
         return self.slug
 
 
+class Author(models.Model):
+    author_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.author_name
