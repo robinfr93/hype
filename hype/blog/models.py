fd4a61ce196a75ff19d0cea1cc9db8db8207007d
robinfr93 <robinfr93@gmail.com>
2015-11-23 21:44:06 +0530

    Created models for Post and Tag whic corresponds to a tag field in Post Model

--- hype/blog/models.py
+++ hype/blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=255, blank=True)
+    create_on = models.DateTimeField(auto_now_add=False)
+
+    def __unicode__(self):
+        return self.tag_name
+
+
+class Post(models.Model):
+    """ Model for post """
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=100)
+    body = models.TextField()
+    published_on = models.DateTimeField(auto_now_add=True)
+    published = models.BooleanField(default=False)
+    slug  = models.SlugField(max_length=255, blank=True)
+    tag = models.ManyToManyField(Tag, related_name="post")
+
+    def __unicode__(self):
+        return self.slug
+
+
+
